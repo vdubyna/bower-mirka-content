6ddc96d9765cb5579ec540ced417eb0b8f5256c3
Volodymyr Dubyna <vdubyna@speroteck.com>
2014-10-04 18:57:22 +0300

    Add deferred functionality Now it could load content async

--- mirka-content.js
+++ mirka-content.js
@@ -13,32 +13,65 @@
    * example:
    * $mkProvider.setContent({info: {description: "Description say Hi!"}});
    * $mkContent.get('info.description');
+   *
+   * It returns promise object and can be used before the content loaded
+   *
+   * example:
+   * in the angular.run()
+   * you can set content loaded async from the server
+   *
+   * AngularApp.run(["$mkContent", function ($mkContent) {
+   *   angular.element.get('data.json', function (data) {
+   *     $mkContent.setContent(data);
+   *   });
+   *
+   *   $rootScope.$content = $mkContent;
+   * }]);
+   *
+   * And in the template just use
+   * <h1>{{ $content.get('messages.hello') }}</h1>
+   *
    */
   function $mkContentProvider() {
-    var contentContainer;
+    var deferred, storage;
 
-    this.setContent = function (content) {
-      contentContainer = content;
-    };
+    function setContent(content) {
+      deferred.resolve(content);
+    }
 
-    this.$get = function () {
-      return {
-        get: function (path) {
-          if (angular.isUndefined(contentContainer)) {
-            throw "Content should be defined before usage";
-          }
-          if (angular.isUndefined(path)) {
-            return contentContainer;
-          } else {
-            return [contentContainer || self]
-              .concat(path.split('.'))
-              .reduce(function (prev, curr) {
-                return prev[curr];
-              });
-          }
+    function getContentManager() {
+      return function (path) {
+        if (angular.isUndefined(storage)) {
+          // Return promise if storage is not filled with content yet
+          return deferred.promise;
         }
-      };
+        if (angular.isUndefined(path)) {
+          // Return Full Content if path not defined
+          return storage;
+        } else {
+          // Return value
+          return [storage || self]
+            .concat(path.split('.'))
+            .reduce(function(prev, curr) {
+              return prev[curr];
+            });
+        }
+      }
+    }
+
+    this.$get = function($q) {
+      deferred = $q.defer();
+      deferred.promise.then(function (content) {
+        storage = content;
+      });
+
+      return {
+        setContent: setContent,
+        get: getContentManager
+      }
     };
+
+    this.setContent = setContent;
   }
 
 })(window, window.angular);
